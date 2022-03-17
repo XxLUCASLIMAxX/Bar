8c8ee6b42c4fbec962e070c06df76ceafae7a874
XxLUCASLIMAxX <lucas-limagomes@hotmail.com>
2022-03-17 17:19:11 -0300

    Base com swagger
    Base do projeto criado e swagger configurado

---
+++ bar/Models/comanda.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace bar.Models
+{
+    public class comanda
+    {
+        [Key]
+        public int codigo { get; set; }
+        public string Name { get; set; }
+
+
+        public double valor        {get;set; }
+
+        public double setValor ()
+        {
+            foreach (var item in itens) { this.valor += item.valor;
+                    
+                    }
+
+            return valor;
+        }
+        public List<item> itens { get; set; }
+
+        
+
+
+    }
+
+
+}
---
+++ bar/Models/item.cs
@@ -0,0 +1,12 @@
+﻿namespace bar.Models
+{
+    public class item
+    {
+
+       public int Id { get; set; }
+       public string Name { get; set; }
+
+       public double valor { get; set; }
+
+    }
+}
